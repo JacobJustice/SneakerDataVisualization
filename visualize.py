52a488388da73aed7f87e9e9023308414ca5554c
JacobJustice <jacobjustice@gmail.com>
2021-03-02 19:23:39 -0500

    polished visualize.py

--- visualize.py
+++ visualize.py
@@ -15,13 +15,13 @@ parser.add_argument('--context', help='Context for seaborn (leave default if you
 
 args = parser.parse_args(sys.argv[1:])
 
-
 sns.set_theme()
 sns.set(style='ticks',rc={'axes.grid':True})
 sns.set_context(args.context)
 
+#rounds to nearest 'base' integer
 def myround(x, base=5):
-    return base * round(x/base)
+    return int(base * round(x/base))
 
 def clean_stockx_dataframe(df):
     # drop duplicates
@@ -39,29 +39,39 @@ def clean_stockx_dataframe(df):
     df['retail_price'] = df['retail_price'].str[1:]
     df['retail_price'] = df['retail_price'].str.replace(',','')
     df['retail_price'] = df['retail_price'].astype(int)
+
+    df['profit'] = df['average_sale_price'] - df['retail_price']
     return df
 
-def plot_profit_histogram(df, binwidth=50):
-    df['profit'] = df['average_sale_price'] - df['retail_price']
+def plot_profit_histogram(df, binwidth=50, xlim_min=-200, xlim_max=1000):
 
-    fig = plt.figure(figsize=(14,9))
+    fig = plt.figure(figsize=(14,10))
     ax = fig.add_subplot(1,1,1)
     ax.xaxis.grid(False)
     ax.set_axisbelow(True)
 #    ax.yaxis.grid(color='gray', linestyle='dashed')
 
     ax.axvline(0,0,1,color='red')
-    n, bins, patches = plt.hist(x=df['profit'], bins=range(myround(df['profit'].min(),binwidth), myround(df['profit'].max() + binwidth,binwidth), binwidth), color='#0504aa',
-                                alpha=.7,range=[df['profit'].min(),df['profit'].max()], edgecolor='black')
+
+    bin_range = range(myround(xlim_min,binwidth), myround(xlim_max+binwidth, binwidth), binwidth)
+    n, bins, patches = plt.hist(x=df['profit'], bins=bin_range, color='#6495ed',
+                                alpha=1,range=[int(df['profit'].min()),int(df['profit'].max())], edgecolor='black')
 
     plt.title ('Frequency of Net Profit Values For ' + name_of_sneaker_model + ' Sneaker Releases (n='+str(len(df['profit']))+')')
     plt.xlabel('Average Net Profit (Difference Between Mean Resale Price and Retail Price) in $USD')
     plt.ylabel('Frequency')
-    plt.xticks(range(myround(df['profit'].min(),binwidth), myround(df['profit'].max() + binwidth,binwidth), binwidth),
-        [get_numeric_string(x) for x in range(myround(df['profit'].min(),binwidth), myround(df['profit'].max() + binwidth,binwidth), binwidth)])
-#    plt.vlines(0,ymin=0,ymax=n.max()+10,colors=['red'])
+
+    xtick_names = [get_numeric_string(x) for x in bin_range]
+    ax.set_xticklabels(xtick_names, rotation=40)
+    ax.set_xticks(bin_range)
+
+    plt.xlim(xlim_min, xlim_max)
+
+    plt.text(700,n.max(), r'$\mu=$' + '{0:.2f}'.format(df['profit'].mean()))
 
     plt.rcParams['axes.axisbelow'] = True
+
+    fig.tight_layout()
 
     plt.draw()
     plt.savefig('profit' + name_of_sneaker_model + '.png')
